9d86aedb402144fd6f65b9712429c080793aed7d
DavidM482 <martin.613901@swl.k12.oh.us>
2024-10-15 08:38:09 -0400

    Initial commit

---
+++ src/LoopsAsCounters.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class LoopsAsCounters {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String num;
+
+        for (int i = 0; i >= 30; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
