eeec735a33f15767c58f9db598e2fdc352396890
DavidM482 <martin.613901@swl.k12.oh.us>
2024-10-15 08:39:15 -0400

    a

--- src/LoopsAsCounters.java
+++ src/LoopsAsCounters.java
@@ -6,7 +6,22 @@ public class LoopsAsCounters {
 
         String num;
 
-        for (int i = 0; i >= 30; i++) {
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 10; i >= 0; i -= 2) {
             System.out.print(i + " ");
         }
     }
